f0098c46da8b84c66df05e78068a86f70bf68cdb
Love Bisaria <bisarialove@gmail.com>
2015-02-10 23:34:44 -0800

    Adding gray code

--- array_string/graycode.c
+++ array_string/graycode.c
@@ -7,3 +7,32 @@
 //
 
 #include <stdio.h>
+int gray_code(int byte1, int byte2);
+
+int main(){
+    int n1, n2;
+    printf("enter the numbers\n");
+    scanf("%d",&n1);
+    scanf("%d",&n2);
+    int retval = gray_code(n1, n2);
+    
+    if(retval == 1){
+        printf("Bytes are gray code\n");
+    }
+    else{
+        printf("Bytes are not gray code\n");
+    }
+}
+
+int gray_code(int byte1, int byte2){
+    
+    int k = byte1^byte2;
+    
+    int and = k&(k-1);
+    
+    if(and == 0){
+        
+        return 1;
+    }
+    return 0;
+}
